ec9053f43fc19eb2b6975774c5fea04dca9f6331
adeel <adeelpv360@gmail.com>
2026-01-21 15:41:16 +0530

    change event columns size to 4

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -19,7 +19,7 @@ const Events = () => {
           </p>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
           {events.map((event) => {            
             return (
               <Link
