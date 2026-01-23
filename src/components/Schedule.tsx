a60a65735d307af862bedf0b6daa35071fb5defb
adeel <adeelpv360@gmail.com>
2026-01-23 20:38:26 +0530

    chore: update time format

--- src/components/Schedule.tsx
+++ src/components/Schedule.tsx
@@ -12,9 +12,9 @@ const scheduleData = [
       { time: "12:00 PM", title: "coding", description: "" },
       { time: "12:00 PM", title: "Prompting", description: "" },
       { time: "12:00 PM", title: "Quiz", description: "" },
-      { time: "1:30 PM", title: "Reel Making", description: "" },
-      { time: "2:30 PM", title: "Blind Typing", description: "" },
-      { time: "2:45 PM", title: "Gaming", description: "" },
+      { time: "01:30 PM", title: "Reel Making", description: "" },
+      { time: "02:30 PM", title: "Blind Typing", description: "" },
+      { time: "02:45 PM", title: "Gaming", description: "" },
     ],
   },
   {
